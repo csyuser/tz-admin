1eb141a2084e1099ec41a730fd7b5c0e76cf0790
csyuser <13540039093@163.com>
2020-11-19 11:09:54 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
+  <div class="about home">
     <h1>This is an about page</h1>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
