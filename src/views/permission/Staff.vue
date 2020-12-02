1fab392b34c847e0b1915bbf9e7cf44133a1fb23
csyuser <13540039093@163.com>
2020-12-02 17:38:25 +0800

    人员增删改查mixin

--- src/views/permission/Staff.vue
+++ src/views/permission/Staff.vue
@@ -12,50 +12,49 @@
            @add="add" @update="update" @postSelect="selectRow"
            @delete="deleteRows" @dblclick="view"></Table>
     <el-dialog :title="dialogTitle" :visible.sync="editDialogVisible" width="970px" :before-close="handleClose">
-      <el-form label-position="right" label-width="85px" :inline="true" :model="staffInfo" size="small" class="addForm"
+      <el-form label-position="right" label-width="85px" :inline="true" :model="editFormInfo" size="small" class="addForm"
                :disabled="editDialogDisabled">
         <el-form-item label="人员名称">
-          <el-input v-model="staffInfo.name" suffix-icon="xxx"></el-input>
+          <el-input v-model="editFormInfo.name" suffix-icon="xxx"></el-input>
         </el-form-item>
         <el-form-item label="人员编码">
-          <el-input v-model="staffInfo.code" suffix-icon="xxx"></el-input>
+          <el-input v-model="editFormInfo.code" suffix-icon="xxx"></el-input>
         </el-form-item>
         <el-form-item label="职务">
-          <el-select v-model="staffInfo.post">
+          <el-select v-model="editFormInfo.post">
             <el-option :label="item['dropName']" :value="item['id']" v-for="item in postDrop" :key="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="职级">
-          <el-select v-model="staffInfo['rank']">
+          <el-select v-model="editFormInfo['rank']">
             <el-option :label="item['dropName']" :value="item['id']" v-for="item in rankDrop" :key="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="性别">
-          <el-select v-model="staffInfo['sex']">
+          <el-select v-model="editFormInfo['sex']">
             <el-option label="女" :value="0"></el-option>
             <el-option label="男" :value="1"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="所属部门" class="departmentItem">
-          <el-input v-model="staffInfo.departmentName" readonly :suffix-icon="iconName" @focus="focusDepartment" @blur="blurDepartment" ref="treeInput"></el-input>
+          <el-input v-model="editFormInfo.departmentName" readonly :suffix-icon="iconName" @focus="focusDepartment" @blur="blurDepartment" ref="treeInput"></el-input>
           <el-tree :data="data" :props="defaultProps" @node-click="selectDepartment" class="tree" :class="{treeVisible}"
                    @node-expand="treeNode" @node-collapse="treeNode"></el-tree>
         </el-form-item>
         <el-form-item label="联系电话">
-          <el-input v-model="staffInfo.phone" suffix-icon="xxx"></el-input>
+          <el-input v-model="editFormInfo.phone" suffix-icon="xxx"></el-input>
         </el-form-item>
         <el-form-item label="联系邮箱">
-          <el-input v-model="staffInfo.email" suffix-icon="xxx"></el-input>
+          <el-input v-model="editFormInfo.email" suffix-icon="xxx"></el-input>
         </el-form-item>
         <el-form-item label="身份证号码">
-          <el-input v-model="staffInfo['idCard']" suffix-icon="xxx"></el-input>
+          <el-input v-model="editFormInfo['idCard']" suffix-icon="xxx"></el-input>
         </el-form-item>
         <el-form-item label="入职时间">
-<!--          <el-input v-model="staffInfo.name" suffix-icon="xxx"></el-input>-->
-          <el-date-picker v-model="staffInfo['entryTime']" type="date" placeholder="选择日期" style="width: 215px"></el-date-picker>
+          <el-date-picker v-model="editFormInfo['entryTime']" type="date" placeholder="选择日期" style="width: 215px"></el-date-picker>
         </el-form-item>
         <el-form-item label="离职时间">
-          <el-date-picker v-model="staffInfo['departureTime']" type="date" placeholder="选择日期" style="width: 215px"></el-date-picker>
+          <el-date-picker v-model="editFormInfo['departureTime']" type="date" placeholder="选择日期" style="width: 215px"></el-date-picker>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -72,7 +71,6 @@
 <script>
 import Table from '@/components/permission/Table'
 import DeleteRow from '@/components/permission/DeleteRow'
-import {helper} from '@/views/method'
 import {mixins} from '@/mixins/mixins'
 
 export default {
@@ -81,29 +79,18 @@ export default {
   mixins:[mixins],
   data() {
     return {
-      page:1,
-      pageSize:10,
       data: [],
       defaultProps: {
         children: 'child',
         label: 'name'
       },
-      colsHead: [{prop: 'name', label: '人员名称'}, {prop: 'code', label: '员工编码'}, {prop: 'post', label: '职务'},
-        {prop: 'rank', label: '职级'}, {prop: 'sex', label: '性别'},{prop: 'departmentName', label: '部门名称'},{prop: 'phone', label: '电话'},
+      colsHead: [{prop: 'name', label: '人员名称'}, {prop: 'code', label: '员工编码'}, {prop: 'postName', label: '职务'},
+        {prop: 'rankName', label: '职级'}, {prop: 'sex', label: '性别'},{prop: 'departmentName', label: '部门名称'},{prop: 'phone', label: '电话'},
         {prop: 'email', label: '电子邮箱'}, {prop: 'idCard', label: '身份证号'}, {prop: 'entryTime', label: '入职时间'},{prop: 'departureTime', label: '离职时间'}],
-      tableDatas: {},
-      dialogType:'',
-      dialogTitle:'',
-      editDialogVisible: false,
-      editDialogDisabled: false,
-      deleteDialogVisible:false,
-      deleteIds:[],
-      selectedRow:[],
-      staffInfo: {},
     }
   },
   mounted() {
-    this.getPages()
+    this.getPages('/person/page')
     this.axios.get(this.prefixAddr + '/department/selectDepartmentTree')
         .then(res => {
           if (res.data.code.toString() === '200') {
@@ -115,111 +102,36 @@ export default {
     this.getDropList('7')
   },
   methods: {
-    getPages(formInline){
-      this.axios.get(this.prefixAddr + '/person/page', {
-        params: {
-          page:this.page,
-          pageSize:this.pageSize,
-          ...formInline
-        },
-      }).then(res => {
-        if (res.data.code.toString() === '200'){
-          this.tableDatas = res.data
-          this.selectedRow = []
-        }else {
-          this.$message.error(res.data.msg)
-        }
-      })
-          .catch()
-    },
     currentChange(val,row){
-      this.page = val
-      this.selectedRow = row
-      this.deleteIds = []
-      this.getPages()
+      this.currentPageChange(val, row, '/person/page')
     },
 //表格增删改查
     selectRow(val) {
-      this.selectedRow = []
-      val.forEach(item => {
-        this.selectedRow.push(item)
-      })
-      console.log(this.selectedRow)
+      this.selectedRows(val)
     },
     search() {
-      this. getPages(this.formInline)
+      this.searchRow('/person/page')
     },
     add() {
-      this.dialogType = 'add'
-      this.dialogTitle = '添加人员'
-      this.staffInfo = {}
-      this.editDialogDisabled = false
-      this.editDialogVisible = true
+      this.dialogTitle = '新增人员'
+      this.addRow()
     },
     update() {
-      this.dialogType = 'update'
-      this.dialogTitle = '编辑人员信息'
-      this.editDialogDisabled = false
-      if (this.selectedRow.length === 1) {
-        this.staffInfo = this.selectedRow[0]
-        this.staffInfo.sex = helper.gender(this.selectedRow[0].sex)
-        this.editDialogVisible = true
-      } else {
-        this.$message.error('请选择一行数据')
-      }
+      this.dialogTitle = '编辑人员'
+      this.updateRow()
     },
     confirmEdit(){
-      this.editDialogVisible = false
-      let editData = {}
-      if (this.dialogType === 'add'){
-        editData = this.staffInfo
-      }else if (this.dialogType === 'update'){editData = {id:this.selectedRow.id,...this.staffInfo}}
-      if (this.dialogType !== 'view'){
-        this.axios.post(this.prefixAddr + '/person/save',{...editData})
-            .then(res=>{
-              if (res.data.code.toString() === '200'){
-                this.$message.success('保存成功')
-                this.getPages()
-              } else this.$message.error(res.data.msg)
-            })
-            .catch()
-      }
+      this.confirmEditRow('/person/save', '/person/page')
     },
     view(row){
-      this.dialogType = 'view'
-      this.dialogTitle = '查看人员信息'
-      this.staffInfo = row
-      this.staffInfo.sex = helper.gender(row.sex)
-      this.editDialogVisible = true
-      this.editDialogDisabled = true
+      this.dialogTitle = '查看权限信息'
+      this.viewRow(row)
     },
     deleteRows() {
-      this.deleteIds = []
-      if (this.selectedRow.length > 0) {
-        this.deleteDialogVisible = true
-        if (this.selectedRow.length > 0) {
-          this.deleteDialogVisible = true
-          this.selectedRow.forEach(row=>{
-            this.deleteIds.push(row.id)
-          })
-        } else {
-          this.$message.error('请选择至少一行数据')
-        }
-      } else {
-        this.$message.error('请选择至少一行数据')
-      }
+      this.deleteRow()
     },
     confirmDelete(){
-      this.deleteDialogVisible = false
-      this.axios.post(this.prefixAddr + '/person/delete',{ids:this.deleteIds})
-          .then(res=>{
-            if (res.data.code.toString() === '200'){
-              this.$message.success('删除成功')
-              this.getPages()
-            }else {this.$message.error(this.data.msg)}
-            console.log(res)
-          })
-          .catch(error=>{this.$message.error('删除失败' + error)})
+      this.confirmDeleteRow('/person/delete', '/person/page')
     },
 //部门的数据下拉框
     treeNode(){
@@ -233,8 +145,8 @@ export default {
     },
     selectDepartment(data) {
       this.selectTree(data)
-      this.staffInfo.departmentName = data.name
-      this.staffInfo.departmentId = data.id
+      this.editFormInfo.departmentName = data.name
+      this.editFormInfo.departmentId = data.id
     },
     handleClose(done) {
       this.$confirm('确认关闭？')
