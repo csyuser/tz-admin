f9f62b604df1d3fb3718946bb76fdbce4f244091
csyuser <13540039093@163.com>
2021-01-05 15:06:27 +0800

    添加login的路由守卫

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ import VueAxios from 'vue-axios'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 let startApp = function () {
   axios.get('/config.json').then((res) => {
     // 基础地址
-    Vue.prototype.BASE_URL = res.data.BASE_URL;
+    Vue.prototype.BASE_URL = res.data.BASE_URL
     Vue.prototype.wsUrl = res.data.wsUrl
     new Vue({
       router,
@@ -25,27 +25,34 @@ let startApp = function () {
   })
 }
 
-function getBseUrl(){
+function getBseUrl() {
   return Vue.prototype.BASE_URL
 }
 
+function LoginAgain() {
+  router.beforeEach((to, from, next) => {
+    if (from.name !== 'Login') next({name: 'Login'})
+    else next()
+  })
+}
+
 axios.interceptors.request.use(function (config) {
-  let token = window.localStorage.getItem('token');
-  config.headers.common['Authorization'] = token ? 'Bearer'+' ' + token :'';
+  let token = window.localStorage.getItem('token')
+  config.headers.common['Authorization'] = token ? 'Bearer' + ' ' + token : ''
   config.baseURL = getBseUrl()
-  return config;
+  return config
 }, function (error) {
-  return Promise.reject(error);
-});
+  return Promise.reject(error)
+})
 axios.interceptors.response.use(function (response) {
-  if (response.data.code && response.data.code.toString() === '-200'){
-    router.push('/')
+  if (response.data.code && response.data.code.toString() === '-200') {
+    LoginAgain()
   }
-  return response;
+  return response
 }, function (error) {
   // 对响应错误做点什么
-  return Promise.reject(error);
-});
+  return Promise.reject(error)
+})
 
 startApp()
 
